4760d230ca3f7d06e533fb8873ed0f0cdbee3131
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2020-03-30 18:05:08 -0300

    unit test for loadData function

--- src/common/loadData.ts
+++ src/common/loadData.ts
@@ -1,10 +1,10 @@
 import Dataloader from 'dataloader'
 
-const { CLEAR_DATALOADER_INTERVAL = '86400000' } = process.env
-
 type OnLoad<T> = (value: string) => Promise<T>
 
 const loadData = <T>(fn: OnLoad<T>) => {
+  const { CLEAR_DATALOADER_INTERVAL = '86400000' } = process.env
+
   const loader = new Dataloader((keys: readonly string[]) => (
     Promise.all(keys.map(fn)
     )))
