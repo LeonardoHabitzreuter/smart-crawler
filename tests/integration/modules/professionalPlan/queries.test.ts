aaf71e7e28ddf57bf1a4c75cd037ba2dbda52fce
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2020-03-30 17:41:51 -0300

    mocked exchanges api for integration test

--- tests/integration/modules/professionalPlan/queries.test.ts
+++ tests/integration/modules/professionalPlan/queries.test.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata'
 import 'dotenv/config'
 import { JSDOM } from 'jsdom'
-import { mockServer } from '../../../helpers/mocks'
+import { mockServer, mockAxios } from '../../../helpers/mocks'
 import { shouldBe } from '../../../helpers/assert'
+
+const { get } = mockAxios()
 
 JSDOM.fromURL = () => JSDOM.fromFile('tests/assets/SmartMEI.html')
 
@@ -22,6 +24,8 @@ const GET_PROFESSIONAL_PLAN = /* GraphQL */ `
 
 describe('professionalPlan query', () => {
   test('should return professional plan data from smartMEI website', async () => {
+    const rates = { EUR: 0.178874877, USD: 0.1963509525 }
+    get.mockReturnValue(Promise.resolve({ data: { rates } }))
     const { query } = mockServer()
 
     const response = await query({
@@ -34,8 +38,11 @@ describe('professionalPlan query', () => {
     const { transferDescription, transferPrice, queryDate } = response.data?.professionalPlan
 
     expect(transferDescription).toBe('Transferência')
-    expect(transferPrice.BRL).toBeGreaterThan(transferPrice.EUR)
-    expect(transferPrice.BRL).toBeGreaterThan(transferPrice.USD)
     shouldBe(new Date(queryDate), new Date())
+    expect(transferPrice).toMatchObject({
+      BRL: 7,
+      EUR: 1.25,
+      USD: 1.37,
+    })
   })
 })
